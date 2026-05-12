2c1af498bbef3840677e387103475eeb575484e5
Kseniya Zhitkova <k.andrushchuk@gmail.com>
2026-05-12 16:17:29 +0300

    2 task

---
+++ src/Task_2_4.java
@@ -0,0 +1,9 @@
+public class Task_2_4 {
+
+    public static void main(String[] args) {
+        System.out.println("Задание 2.4");
+        int a;
+        a = (98 - 25) * 6;
+        System.out.println("РЕЗУЛЬТАТ:" + a);
+    }
+}
---
+++ src/Task_2_6.java
@@ -0,0 +1,8 @@
+public class Task_2_6 {
+    public static void main(String[] args) {
+        int a = 35;
+        int b = 65;
+        int c = a > b ? 5 : 10;
+        System.out.println(c);
+    }
+}
