eeb4aea215159ee13165c44d8d7451a140a0e4ee
Kseniya Zhitkova <k.andrushchuk@gmail.com>
2026-05-17 20:14:10 +0300

    2 task

--- src/task5/Task_4.java
+++ src/task5/Task_4.java
@@ -1,5 +1,34 @@
 package task5;
 
-public class Task_4 {
+import java.util.Scanner;
 
+public class Task_4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число не заканчивающееся на 0:");
+        int x = scanner.nextInt();
+        if (x % 10 == 0) {
+            System.out.println("Вы ввели число заканчивающееся на 0");
+            scanner.close();
+            return;
+        }
+        int temp = x;
+        System.out.println("Чётные и нечётные цифры числа " + x + ":");
+        while (temp > 0) {
+            int digit = temp % 10;
+            if (digit % 2 == 0) {
+                System.out.println(digit + " четное");
+            } else {
+                System.out.println(digit + " нечетное");
+            }
+            temp = temp / 10;
+        }
+        scanner.close();
+    }
 }
+
+
+
+
+
+
